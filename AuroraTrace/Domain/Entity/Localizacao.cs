0d1236b8c04506aa85faf4efd2c8c5a537a8d363
samueldamasceno <samueldamasceno.vip@gmail.com>
2025-05-15 17:41:32 -0300

    classe para localização criada

---
+++ AuroraTrace/Domain/Entity/Localizacao.cs
@@ -0,0 +1,27 @@
+﻿using AuroraTrace.Domain.Exceptions;
+
+namespace AuroraTrace.Domain.Entity
+{
+    public class Localizacao
+    {
+        public Guid Id { get; private set; }
+        public double Latitude { get; private set; }
+        public double Longitude { get; private set; }
+        public DateTime RegistradaEm { get; private set; }
+
+        private Localizacao(double latitude, double longitude)
+        {
+            Id = Guid.NewGuid();
+            Latitude = latitude;
+            Longitude = longitude;
+            RegistradaEm = DateTime.UtcNow;
+        }
+
+        internal static Localizacao Create(double latitude, double longitude)
+        {
+            return new Localizacao(latitude, longitude);
+        }
+
+        public Localizacao() { }
+    }
+}
