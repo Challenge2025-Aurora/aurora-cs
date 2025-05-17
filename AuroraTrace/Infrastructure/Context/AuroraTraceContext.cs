6a626261509194944c9fa7fbb4eaaa043d3e5bbb
samueldamasceno <samueldamasceno.vip@gmail.com>
2025-05-17 16:59:27 -0300

    atualização

--- AuroraTrace/Infrastructure/Context/AuroraTraceContext.cs
+++ AuroraTrace/Infrastructure/Context/AuroraTraceContext.cs
@@ -11,6 +11,8 @@ namespace AuroraTrace.Infrastructure.Context
         public DbSet<Camera> Cameras { get; set; }
         public DbSet<Patio> Patios { get; set; }
         public DbSet<Localizacao> Localizacoes { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -19,6 +21,9 @@ namespace AuroraTrace.Infrastructure.Context
             modelBuilder.ApplyConfiguration(new CameraMapping());
             modelBuilder.ApplyConfiguration(new PatioMapping());
             modelBuilder.ApplyConfiguration(new LocalizacaoMapping());
+            modelBuilder.ApplyConfiguration(new  FuncionarioMapping());
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
--- AuroraTrace/Program.cs
+++ AuroraTrace/Program.cs
@@ -1,23 +1,41 @@
+using AuroraTrace.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(swagger =>
+{
+    swagger.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = builder.Configuration["Swagger:Title"],
+        Description = builder.Configuration["Swagger:Description"],
+        Contact = new OpenApiContact
+        {
+            Name = builder.Configuration["Swagger:Contact:Name"],
+            Email = builder.Configuration["Swagger:Contact:Email"]
+        }
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    swagger.IncludeXmlComments(xmlPath);
+});
+
+builder.Services.AddDbContext<AuroraTraceContext>(options =>
+    options.UseOracle(builder.Configuration.GetConnectionString("DefaultConnection"))
+           .UseLazyLoadingProxies());
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
