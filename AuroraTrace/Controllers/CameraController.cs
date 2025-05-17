a7296e7792e6d5910a44f42dabee09fb5addd5ab
samueldamasceno <samueldamasceno.vip@gmail.com>
2025-05-17 17:37:09 -0300

    método get adicionado pras outras entidades

---
+++ AuroraTrace/Controllers/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using AuroraTrace.Domain.Entity;
+using AuroraTrace.Infrastructure.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuroraTrace.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CameraController : ControllerBase
+    {
+        private readonly AuroraTraceContext _context;
+
+        public CameraController(AuroraTraceContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Camera>>> GetAll()
+        {
+            var cameras = await _context.Cameras.ToListAsync();
+            return Ok(cameras);
+        }
+    }
+}
---
+++ AuroraTrace/Controllers/LocalizacaoController.cs
@@ -0,0 +1,26 @@
+﻿using AuroraTrace.Domain.Entity;
+using AuroraTrace.Infrastructure.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuroraTrace.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LocalizacaoController : ControllerBase
+    {
+        private readonly AuroraTraceContext _context;
+
+        public LocalizacaoController(AuroraTraceContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Localizacao>>> GetAll()
+        {
+            var localizacoes = await _context.Localizacoes.ToListAsync();
+            return Ok(localizacoes);
+        }
+    }
+}
