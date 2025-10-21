2e66ac7264cf8172e69660fd9dcdc9a778692283
samueldamasceno <sdamasceno756@gmail.com>
2025-10-21 01:43:07 -0300

    refactor(): alterar tipo dos atributos da entidade 'moto' para usar novo value object

--- src/Domain/Entity/Moto.cs
+++ src/Domain/Entity/Moto.cs
@@ -2,28 +2,28 @@
 
 using Domain.Exceptions;
 using Domain.Enum;
+using Domain.ValueObject;
 using System;
 
 public class Moto
 {
     public long Id { get; private set; }
-    public string Placa { get; private set; } = null!;
+    public Placa Placa { get; private set; } = null!;
     public string Modelo { get; private set; } = null!;
     public StatusMoto Status { get; private set; }
     public DateTime AtualizadoEm { get; private set; }
     public DateTime DataCadastro { get; private set; }
-
     public string? UltimoSetor { get; private set; }
     public string? UltimoSlot { get; private set; }
 
-    private Moto(string placa, string modelo, StatusMoto status, string? setor, string? slot)
+    private Moto(Placa placa, string modelo, StatusMoto status, string? setor, string? slot)
     {
-        if (string.IsNullOrWhiteSpace(placa))
+        if (placa is null || string.IsNullOrWhiteSpace(placa.Valor))
             throw new DomainException("Placa é obrigatória");
         if (string.IsNullOrWhiteSpace(modelo))
             throw new DomainException("Modelo é obrigatório");
 
-        Placa = placa.ToUpper();
+        Placa = placa;
         Modelo = modelo;
         Status = status;
         AtualizadoEm = DateTime.UtcNow;
@@ -32,10 +32,8 @@ public class Moto
         UltimoSlot = slot;
     }
 
-    internal static Moto Create(string placa, string modelo, StatusMoto status, string? setor, string? slot)
-    {
-        return new Moto(placa, modelo, status, setor, slot);
-    }
+    public static Moto Create(Placa placa, string modelo, StatusMoto status, string? setor, string? slot)
+        => new Moto(placa, modelo, status, setor, slot);
 
     public void AtualizarStatus(StatusMoto novoStatus, string? setor = null, string? slot = null)
     {
---
+++ src/Domain/ValueObject/Placa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Domain.ValueObject
+{
+    public class Placa : IEquatable<Placa>
+    {
+        public string Valor { get; }
+
+        public Placa(string valor)
+        {
+            if (!Regex.IsMatch(valor, @"^[A-Z]{3}[0-9][0-9A-Z][0-9]{2}$"))
+                throw new ArgumentException("Placa inválida.");
+
+            Valor = valor.ToUpper();
+        }
+
+        public override bool Equals(object? obj) => Equals(obj as Placa);
+
+        public bool Equals(Placa? other) => other != null && Valor == other.Valor;
+
+        public override string ToString() => Valor;
+    }
+
+}
