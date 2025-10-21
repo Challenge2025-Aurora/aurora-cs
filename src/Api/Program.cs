a90795d02616d2b81d603c74c55fd239e6041e79
samueldamasceno <sdamasceno756@gmail.com>
2025-10-21 05:19:51 -0300

    fix(): corrigir dependências e fazer projeto rodar

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,30 +1,29 @@
 using Application.Services;
 using AutoMapper;
 using Infrastructure.Context;
+using Infrastructure.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
+using MongoDB.Driver;
+using Domain.Repositories;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAutoMapper(cfg =>
-{
-    cfg.AddProfile<MappingProfile>();
-});
-
+builder.Services.AddAutoMapper(cfg => cfg.AddProfile<MappingProfile>());
 builder.Services.AddControllers();
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-        policy =>
-        {
-            policy.AllowAnyOrigin()
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-        });
+    options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
 });
 
 builder.Services.AddApiVersioning(options =>
@@ -33,43 +32,25 @@ builder.Services.AddApiVersioning(options =>
     options.DefaultApiVersion = new ApiVersion(1, 0);
     options.ReportApiVersions = true;
 });
-
 builder.Services.AddVersionedApiExplorer(options =>
 {
     options.GroupNameFormat = "'v'VVV";
     options.SubstituteApiVersionInUrl = true;
 });
 
-builder.Services.AddSwaggerGen(options =>
-{
-    var provider = builder.Services.BuildServiceProvider()
-                        .GetRequiredService<IApiVersionDescriptionProvider>();
+builder.Services.AddSwaggerGen(o => { o.EnableAnnotations(); });
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
 
-    foreach (var description in provider.ApiVersionDescriptions)
-    {
-        options.SwaggerDoc(description.GroupName, new OpenApiInfo
-        {
-            Title = $"AuroraTrace API {description.ApiVersion}",
-            Version = description.ApiVersion.ToString(),
-            Description = "API para gerenciamento AuroraTrace",
-            Contact = new OpenApiContact
-            {
-                Name = builder.Configuration["Swagger:Contact:Name"],
-                Email = builder.Configuration["Swagger:Contact:Email"]
-            }
-        });
-    }
-
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    if (File.Exists(xmlPath))
-        options.IncludeXmlComments(xmlPath);
-});
-
-builder.Services.AddHealthChecks()
-    .AddMongoDb(builder.Configuration.GetConnectionString("MongoDB"), name: "mongodb");
-
+var mongoConnectionString = builder.Configuration.GetConnectionString("MongoDB");
+builder.Services.AddSingleton<IMongoClient>(_ => new MongoClient(mongoConnectionString));
 builder.Services.AddSingleton<AuroraMongoContext>();
+builder.Services.AddHealthChecks().AddMongoDb(sp => sp.GetRequiredService<IMongoClient>(), name: "mongodb");
+
+builder.Services.AddScoped<IMotoRepository, MotoRepository>();
+builder.Services.AddScoped<IPatioRepository, PatioRepository>();
+builder.Services.AddScoped<ISetorRepository, SetorRepository>();
+builder.Services.AddScoped<IEventoRepository, EventoRepository>();
+builder.Services.AddScoped<IDeteccaoRepository, DeteccaoRepository>();
 
 builder.Services.AddScoped<MotoService>();
 builder.Services.AddScoped<PatioService>();
