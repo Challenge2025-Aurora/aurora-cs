cc0f86b8de5a37837d0802eb57802357ff106813
samueldamasceno <sdamasceno756@gmail.com>
2025-10-21 01:47:51 -0300

    feat(database): implementar mongodb ao projeto, substituindo ef core

---
+++ src/Infrastructure/Context/AuroraMongoContext.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using Domain.Entity;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Context
+{
+    public class AuroraMongoContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public AuroraMongoContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetConnectionString("MongoDb"));
+            _database = client.GetDatabase("AuroraTraceDb");
+        }
+
+        public IMongoCollection<Moto> Motos => _database.GetCollection<Moto>("Motos");
+        public IMongoCollection<Patio> Patios => _database.GetCollection<Patio>("Patios");
+        public IMongoCollection<Setor> Setores => _database.GetCollection<Setor>("Setores");
+        public IMongoCollection<Evento> Eventos => _database.GetCollection<Evento>("Eventos");
+        public IMongoCollection<Deteccao> Deteccoes => _database.GetCollection<Deteccao>("Deteccoes");
+    }
+}
---
+++ src/Infrastructure/Mongo/MongoDBService.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entity;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Infrastructure.Mongo
+{
+    public class MongoDBService
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDBService(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetConnectionString("MongoDB"));
+            _database = client.GetDatabase("AuroraDb");
+        }
+
+        public IMongoCollection<Moto> Motos => _database.GetCollection<Moto>("Motos");
+        public IMongoCollection<Patio> Patios => _database.GetCollection<Patio>("Patios");
+        public IMongoCollection<Setor> Setores => _database.GetCollection<Setor>("Setores");
+        public IMongoCollection<Evento> Eventos => _database.GetCollection<Evento>("Eventos");
+        public IMongoCollection<Deteccao> Deteccoes => _database.GetCollection<Deteccao>("Deteccoes");
+    }
+}
