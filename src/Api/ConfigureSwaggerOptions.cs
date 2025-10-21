348f974967aafabfd60931aaa5620d11ffd65ed1
samueldamasceno <sdamasceno756@gmail.com>
2025-10-21 05:14:55 -0300

    refactor(swagger): atualizar swagger separando da lógica principal

---
+++ src/Api/ConfigureSwaggerOptions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Reflection;
+
+public sealed class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+{
+    private readonly IApiVersionDescriptionProvider _provider;
+    private readonly IConfiguration _configuration;
+
+    public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider, IConfiguration configuration)
+    {
+        _provider = provider;
+        _configuration = configuration;
+    }
+
+    public void Configure(SwaggerGenOptions options)
+    {
+        options.EnableAnnotations();
+
+        foreach (var desc in _provider.ApiVersionDescriptions)
+        {
+            options.SwaggerDoc(desc.GroupName, new OpenApiInfo
+            {
+                Title = $"AuroraTrace API {desc.ApiVersion}",
+                Version = desc.ApiVersion.ToString(),
+                Description = "API para gerenciamento AuroraTrace",
+                Contact = new OpenApiContact
+                {
+                    Name = _configuration["Swagger:Contact:Name"],
+                    Email = _configuration["Swagger:Contact:Email"]
+                }
+            });
+        }
+
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        if (File.Exists(xmlPath)) options.IncludeXmlComments(xmlPath);
+    }
+}
