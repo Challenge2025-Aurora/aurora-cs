8a44b0a55ea17af91f98f8da6caf819912f7d9f3
samueldamasceno <sdamasceno756@gmail.com>
2025-11-09 02:48:54 -0300

    feat(tests): testes com xunity criados e funcionando

---
+++ src/Api/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using Infrastructure.Security;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly JwtService _jwtService;
+
+    public AuthController(JwtService jwtService)
+    {
+        _jwtService = jwtService;
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] string userId)
+    {
+        var token = _jwtService.GenerateToken(userId);
+        return Ok(new { token });
+    }
+}
